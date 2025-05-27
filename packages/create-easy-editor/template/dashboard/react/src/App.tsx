533a38f05173ef6ce4e3c14eff8ac311c8399c7a
JinSo <kimjinso@qq.com>
2025-05-27 18:10:42 +0800

    perf: remove unsed pkgs

--- packages/create-easy-editor/template/dashboard/react/src/App.tsx
+++ packages/create-easy-editor/template/dashboard/react/src/App.tsx
@@ -1,5 +1,4 @@
 import { lazy } from 'react'
-import { ErrorBoundary } from 'react-error-boundary'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router'
 import { ThemeProvider } from './components/theme-provider'
 
@@ -8,19 +7,15 @@ const Editor = lazy(() => import('./pages/editor'))
 
 function App() {
   return (
-    <ErrorBoundary fallback={<div className='p-4 text-red-500'>严重错误！请刷新页面</div>}>
-      <ThemeProvider defaultTheme='system' storageKey='easy-dashboard-theme'>
-        <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<Editor />} />
-            <Route path='/preview' element={<Preview />} />
-            <Route path='/test' element={<div>test</div>} />
-
-            <Route path='*' element={<Navigate to='/' />} />
-          </Routes>
-        </BrowserRouter>
-      </ThemeProvider>
-    </ErrorBoundary>
+    <ThemeProvider defaultTheme='system' storageKey='easy-dashboard-theme'>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Editor />} />
+          <Route path='/preview' element={<Preview />} />
+          <Route path='*' element={<Navigate to='/' />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
