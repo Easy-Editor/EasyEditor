c451d73f5e0a27d948703cbab763909de70574e1
JinSo <kimjinso@qq.com>
2025-05-27 18:34:14 +0800

    fix: create-easy-editor build error

--- packages/create-easy-editor/rollup.config.js
+++ packages/create-easy-editor/rollup.config.js
@@ -1,26 +1,12 @@
 import babel from '@rollup/plugin-babel'
-import nodeResolve from '@rollup/plugin-node-resolve'
-import { createRequire } from 'node:module'
-import cleanup from 'rollup-plugin-cleanup'
-
-const require = createRequire(import.meta.url)
-const pkg = require('./package.json')
 
 const plugins = [
-  nodeResolve({
-    extensions: ['.js', '.ts'],
-    browser: true,
-  }),
   babel({
     extensions: ['.js', '.ts'],
     exclude: 'node_modules/**',
     babelrc: false,
     babelHelpers: 'bundled',
     presets: ['@babel/preset-typescript'],
-  }),
-  cleanup({
-    comments: ['some', /PURE/],
-    extensions: ['.js', '.ts'],
   }),
 ]
 
