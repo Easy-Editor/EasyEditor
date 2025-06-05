92a47dce519703d2fcbbe85671df3563141297b4
JinSo <kimjinso@qq.com>
2025-06-05 12:51:00 +0800

    fix: perfrom DataSource

--- packages/plugin-datasource/src/type.ts
+++ packages/plugin-datasource/src/type.ts
@@ -1,10 +1,30 @@
-import type { DataSource } from '@easy-editor/core'
+import type { InterpretDataSource } from './types'
 
 declare module '@easy-editor/core' {
-  interface Project {
+  interface ProjectSchema {
     /**
      * 数据源配置
      */
-    dataSource: DataSource | undefined
+    // @ts-ignore
+    dataSource?: InterpretDataSource
+  }
+
+  interface RootSchema {
+    /**
+     * 数据源配置
+     */
+    // @ts-ignore
+    dataSource?: InterpretDataSource
+  }
+
+  interface DataSourceEngine {
+    // @ts-ignore
+    createDataSourceEngine: (
+      dataSource: InterpretDataSource,
+      engine: any,
+    ) => {
+      dataSourceMap: Record<string, any>
+      reloadDataSource: () => Promise<void>
+    }
   }
 }
